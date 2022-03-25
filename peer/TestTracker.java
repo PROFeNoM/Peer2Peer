a1aca31f18f201e55cb54b6e6df0d00a0e3014ba
agaudy001 <antoine.gaudy@enseirb-matmeca.fr>
2022-03-25 09:31:39 +0100

    changes on Tracker.java

--- peer/TestTracker.java
+++ peer/TestTracker.java
@@ -1,7 +1,17 @@
-
+import java.io.IOException;
+import java.util.concurrent.TimeUnit ;
 class TestTracker {
     public static void main(String[] args) {
         Tracker tracker = new Tracker();
         tracker.start(Integer.parseInt(args[0]));
+        int nb_messages = 5;
+        try {
+            for (int i = 0; i < nb_messages; i++) {
+                TimeUnit.SECONDS.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
