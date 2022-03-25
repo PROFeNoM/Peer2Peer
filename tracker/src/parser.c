aaa91757105938ead9c90612b0c82597285e1dcf
Alexandre Choura <alexandre.choura@bordeaux-inp.fr>
2022-03-25 11:59:46 +0100

    Recognize the format of an "announce" request + its tests

--- tracker/src/parser.c
+++ tracker/src/parser.c
@@ -4,47 +4,39 @@
 
 #include "include/parser.h"
 
-#define UNUSED(x) (void)(x)
+#define KNOWN_REQUEST_REGEX "^(announce|look|getfile|update).*"
+#define ANNOUNCE_REQUEST_REGEX "^announce listen [0-9]*\\( seed \\[\\(.* [0-9]\\+ [0-9]\\+ \\w\\+\\s\\?\\)\\+\\]\\)\\?\\( leech \\[.*\\]\\)\\?$"
 
-#define KNOWN_REQUEST_REGEX "^\b(announce|look|getfile|update)\b"
-
-void error_tmp(char* msg)
-{
-	perror(msg);
-	exit(1);
+void error_tmp(char *msg) {
+    perror(msg);
+    exit(1);
 }
 
 /*
  * Check if the request is among the known request types
  * @param request - Request to check
  */
-int is_request_type_known(char* request)
-{
-	regex_t regex;
-	int reti;
+int is_request_type_known(char *request) {
+    regex_t regex;
+    int reti;
 
-	// Compile regular expression
-	reti = regcomp(&regex, KNOWN_REQUEST_REGEX, 0);
-	if (reti) error_tmp("Error compiling regex\n");
+    // Compile regular expression
+    reti = regcomp(&regex, KNOWN_REQUEST_REGEX, REG_EXTENDED);
+    if (reti) error_tmp("Error compiling regex\n");
 
-	// Execute regular expression
-	reti = regexec(&regex, request, 0, NULL, 0);
-	if (!reti)
-	{
-		regfree(&regex);
-		return 1;
-	}
-	else if (reti == REG_NOMATCH)
-	{
-		regfree(&regex);
-		return 0;
-	}
-	else
-	{
-		regfree(&regex);
-		error_tmp("Error executing regex\n");
-		return 0;
-	}
+    // Execute regular expression
+    reti = regexec(&regex, request, 0, NULL, 0);
+    if (!reti) {
+        regfree(&regex);
+        return 1;
+    } else if (reti == REG_NOMATCH) {
+        regfree(&regex);
+        return 0;
+    } else {
+        regfree(&regex);
+        error_tmp("Error executing regex\n");
+        return 0;
+    }
 }
 
 /*
@@ -52,32 +44,60 @@ int is_request_type_known(char* request)
  * @param request - Request to check
  * @param request_t - Request type
  */
-int is_request_valid(char* request, enum REQUEST_T request_t) {
-	UNUSED(request);
-	UNUSED(request_t);
-	return 0;
+int is_request_valid(char *request, enum REQUEST_T request_t) {
+    regex_t regex;
+    int reti;
+
+    // Compile regular expression
+    switch (request_t) {
+        case ANNOUNCE:
+            reti = regcomp(&regex, ANNOUNCE_REQUEST_REGEX, 0);
+            break;
+        case LOOK:
+            break;
+        case GETFILE:
+            break;
+        case UPDATE:
+            break;
+        case INVALID:
+        case UNKNOWN:
+            break;
+    }
+    if (reti) error_tmp("Error compiling regex\n");
+
+    // Execute regular expression
+    reti = regexec(&regex, request, 0, NULL, 0);
+    if (!reti) {
+        regfree(&regex);
+        return 1;
+    } else if (reti == REG_NOMATCH) {
+        regfree(&regex);
+        return 0;
+    } else {
+        regfree(&regex);
+        error_tmp("Error executing regex\n");
+        return 0;
+    }
 }
 
-enum REQUEST_T get_request_type(char* request)
-{
-	if (!is_request_type_known(request)) return UNKNOWN;
+enum REQUEST_T get_request_type(char *request) {
+    if (!is_request_type_known(request)) return UNKNOWN;
 
-	enum REQUEST_T potential_request_type;
-	switch (request[0])
-	{
-	case 'a':
-		potential_request_type = ANNOUNCE;
-		break;
-	case 'l':
-		potential_request_type = LOOK;
-		break;
-	case 'g':
-		potential_request_type = GETFILE;
-		break;
-	case 'u':
-		potential_request_type = UPDATE;
-		break;
-	}
+    enum REQUEST_T potential_request_type;
+    switch (request[0]) {
+        case 'a':
+            potential_request_type = ANNOUNCE;
+            break;
+        case 'l':
+            potential_request_type = LOOK;
+            break;
+        case 'g':
+            potential_request_type = GETFILE;
+            break;
+        case 'u':
+            potential_request_type = UPDATE;
+            break;
+    }
 
-	return is_request_valid(request, potential_request_type) ? potential_request_type : INVALID;
+    return is_request_valid(request, potential_request_type) ? potential_request_type : INVALID;
 }
--- tracker/src/tests/test_parser.c
+++ tracker/src/tests/test_parser.c
@@ -3,12 +3,111 @@
 
 int test__get_request_type_with_unknown_request()
 {
-	ASSERT_EQUAL(get_request_type(""), UNKNOWN);
+	ASSERT_EQUAL(UNKNOWN, get_request_type(""))
 
 	return 1;
 }
 
+int test__get_request_type_with_valid_seed_only_announce_request()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966 file_b.dat 3145728 1536 330a57722ec8b0bf09669a2b35f88e9e]";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(ANNOUNCE, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_valid_seed_only_announce_request_one_file()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966]";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(ANNOUNCE, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_invalid_seed_only_announce_request_incomplete()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966 file_b.dat]";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(INVALID, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_valid_seed_leech_announce_request()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966 file_b.dat 3145728 1536 330a57722ec8b0bf09669a2b35f88e9e] leech [8905e92afeb80fc7722ec89eb0bf0966 330a57722ec8b0bf09669a2b35f88e9e]";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(ANNOUNCE, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_valid_seed_empty_leech_array_announce_request()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966 file_b.dat 3145728 1536 330a57722ec8b0bf09669a2b35f88e9e] leech []";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(ANNOUNCE, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_invalid_seed_empty_leech_announce_request()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966 file_b.dat 3145728 1536 330a57722ec8b0bf09669a2b35f88e9e] leech ";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(INVALID, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_invalid_seed_leech_announce_request_double_spaces_between_files()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024 8905e92afeb80fc7722ec89eb0bf0966  file_b.dat 3145728 1536 330a57722ec8b0bf09669a2b35f88e9e] leech ";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(INVALID, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_invalid_seed_leech_announce_request_double_spaces_between_fields()
+{
+    char request[] = "announce listen 2222 seed [file_a.dat 2097152 1024  8905e92afeb80fc7722ec89eb0bf0966 file_b.dat 3145728 1536 330a57722ec8b0bf09669a2b35f88e9e] leech ";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(INVALID, actual)
+
+    return 1;
+}
+
+int test__get_request_type_with_valid_leech_only_announce_request()
+{
+    char request[] = "announce listen 2222 leech [8905e92afeb80fc7722ec89eb0bf0966 330a57722ec8b0bf09669a2b35f88e9e]";
+
+    enum REQUEST_T actual = get_request_type(request);
+    ASSERT_EQUAL(ANNOUNCE, actual)
+
+    return 1;
+}
+
 void test__parser_functions()
 {
-	TEST_FUNCTION(test__get_request_type_with_unknown_request);
+	TEST_FUNCTION(test__get_request_type_with_unknown_request)
+    TEST_FUNCTION(test__get_request_type_with_valid_seed_only_announce_request)
+    TEST_FUNCTION(test__get_request_type_with_valid_seed_only_announce_request_one_file)
+    TEST_FUNCTION(test__get_request_type_with_invalid_seed_only_announce_request_incomplete)
+    TEST_FUNCTION(test__get_request_type_with_valid_seed_leech_announce_request)
+    TEST_FUNCTION(test__get_request_type_with_valid_seed_empty_leech_array_announce_request)
+    TEST_FUNCTION(test__get_request_type_with_invalid_seed_empty_leech_announce_request)
+    TEST_FUNCTION(test__get_request_type_with_invalid_seed_leech_announce_request_double_spaces_between_files)
+    TEST_FUNCTION(test__get_request_type_with_invalid_seed_leech_announce_request_double_spaces_between_fields)
+    TEST_FUNCTION(test__get_request_type_with_valid_leech_only_announce_request)
 }
