4ff48d68dc4159f2e8991ac15cfdb8b4975413ee
theogom <theo.gom.2000@gmail.com>
2022-04-08 09:08:18 +0200

    Update configuration file and optimize configuration class

--- peer/src/Configuration.java
+++ peer/src/Configuration.java
@@ -4,27 +4,40 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class Configuration {
-    final static String CONFIG_FILE = "config.ini";
+    private static Configuration instance;
+    private final String CONFIG_FILE = "config.ini";
+    private final Properties prop = new Properties();
 
-    private static String getProperty(String key) {
-        Properties prop = new Properties();
+    private Configuration() {
         try {
             prop.load(new FileInputStream(CONFIG_FILE));
+
         } catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-            System.exit(-1);
+            System.out.println("Cannot find config file: " + e.getMessage());
+            System.exit(1);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
-            System.exit(-1);
+            System.out.println("Cannot read config file: " + e.getMessage());
+            System.exit(1);
         }
-        return prop.getProperty(key);
+        System.out.println("Configuration file " + CONFIG_FILE + " loaded");
     }
 
-    public static int getPort() {
-        return Integer.parseInt(getProperty("peer-port"));
+    public static Configuration getInstance() {
+        if (instance == null) {
+            instance = new Configuration();
+        }
+        return instance;
     }
 
-    public static String getIp() {
-        return getProperty("peer-ip");
+    public String getTrackerIp() {
+        return prop.getProperty("tracker-ip");
+    }
+
+    public int getTrackerPort() {
+        return Integer.parseInt(prop.getProperty("tracker-port"));
+    }
+
+    public int getPeerPort() {
+        return Integer.parseInt(prop.getProperty("peer-port"));
     }
 }
