e2a43364c62b7ab7dc6bb0d4339dc4e599c83029
agaudy001 <antoine.gaudy@enseirb-matmeca.fr>
2022-04-15 10:56:30 +0200

    computeBufferMap function, but has to be reversed

--- peer/src/main/Seed.java
+++ peer/src/main/Seed.java
@@ -1,12 +1,14 @@
 package peer.src.main;
 
+import java.util.*;
 import peer.src.main.util.FileHandler;
 import java.io.*;
 
 public class Seed {
     String key;
-    String buffermap;
+    ArrayList<Integer> buffermap;
     File file;
+    int pieceSize = 1024;
 
     public Seed(String path) {
         this.file = openFile(path);
@@ -27,7 +29,21 @@ public class Seed {
         return file;
     }
 
-    public String computeBufferMap() {
-        return "";
+    // A function that returns the values where bit number are at 1
+    public ArrayList<Integer> computeBufferMap() {
+        int bufferMap = 1457;
+        int bufferSize = (int) Math.ceil((double) file.length() / pieceSize);
+        ArrayList<Integer> bufferMaped = new ArrayList<Integer>();
+        while (bufferMap > 0) {
+            if ((bufferMap % 2) == 1) {
+                bufferMaped.add(1);
+            }else {
+                bufferMaped.add(0);
+            }
+            System.out.println(bufferMaped);
+            bufferMap >>= 1;
+        }
+        return bufferMaped; // trouver un moyen de reverse la liste
+        // return Collections.reverse(bufferMaped);
     }
 }
