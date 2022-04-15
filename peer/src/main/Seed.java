91cc114dd207f4d8b0858a2e3247e4847d5ed8cc
theogom <theo.gom.2000@gmail.com>
2022-04-15 09:32:02 +0200

    Add Seed class

---
+++ peer/src/main/Seed.java
@@ -0,0 +1,33 @@
+package peer.src.main;
+
+import peer.src.main.util.FileHandler;
+import java.io.*;
+
+public class Seed {
+    String key;
+    String buffermap;
+    File file;
+
+    public Seed(String path) {
+        this.file = openFile(path);
+        this.key = FileHandler.getHash(this.file);
+        this.buffermap = computeBufferMap();
+    }
+
+    public String getName() {
+        return file.getName();
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    private File openFile(String path) {
+        File file = new File(path);
+        return file;
+    }
+
+    public String computeBufferMap() {
+        return "";
+    }
+}
