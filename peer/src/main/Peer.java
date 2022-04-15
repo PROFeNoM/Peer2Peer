f3568a903f314a0327a17f158f4adb50c6ecd36f
theogom <theo.gom.2000@gmail.com>
2022-04-15 08:11:44 +0200

    Announce to tracker automatically at launch

--- peer/src/main/Peer.java
+++ peer/src/main/Peer.java
@@ -19,6 +19,7 @@ public class Peer {
     public void start(String trackerIp, int trackerPort, int peerPort) {
         connectToTracker(trackerIp, trackerPort);
         startServer(peerPort);
+        announceToTracker(peerPort);
     }
 
     // Connect to the tracker on the given ip and port
@@ -34,7 +35,35 @@ public class Peer {
             System.out.println("Cannot connect to tracker: " + e.getMessage());
             System.exit(1);
         }
-        System.out.println("Connected to tracker on port " + port);
+    }
+
+    // Send the initial announce message to the tracker
+    public void announceToTracker(int peerPort) {
+        String message = "announce";
+        message += " listen " + peerPort;
+        message += " seed " + getSeed();
+        message += " leech " + getLeech();
+        trackerOut.println(message);
+
+        try {
+            String response = trackerIn.readLine();
+            if (response.equals("ok")) {
+                System.out.println("Announced to tracker");
+            } else {
+                System.out.println("Failed to announce to tracker: " + response);
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to announce to tracker: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public String getSeed() {
+        return "[]";
+    }
+
+    public String getLeech() {
+        return "[]";
     }
 
     // Start a server on given port
@@ -43,6 +72,7 @@ public class Peer {
             System.out.println("Server already started");
             return;
         }
+        System.out.println("Starting server on port " + port);
 
         try {
             serverSocket = new ServerSocket(port);
@@ -85,6 +115,11 @@ public class Peer {
         }
     }
 
+    // Send a message either to the tracker or to the peers
+    public String sendMessage(String msg) {
+        return "";
+    }
+
     // Close the connection to the tracker and stop the server
     public void stop() {
         try {
@@ -111,40 +146,39 @@ public class Peer {
         return response;
     }
 
-        // Connect to a peer on the given ip and port
-        public void connectToPeer(String ip, int port) {
-            try {
-                clientSocket = new Socket(ip, port);
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            } catch (UnknownHostException e) {
-                System.out.println("Cannot connect to peer: " + e.getMessage());
-                System.exit(1);
-            } catch (IOException e) {
-                System.out.println("Cannot connect to peer: " + e.getMessage());
-                System.exit(1);
-            }
-            System.out.println("Connected to peer on port " + port);
+    // Connect to a peer on the given ip and port
+    public void connectToPeer(String ip, int port) {
+        try {
+            clientSocket = new Socket(ip, port);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        } catch (UnknownHostException e) {
+            System.out.println("Cannot connect to peer: " + e.getMessage());
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("Cannot connect to peer: " + e.getMessage());
+            System.exit(1);
         }
+        System.out.println("Connected to peer on port " + port);
+    }
 
     void ProcessResponse() {
         java.lang.reflect.Method method;
         try {
             method = this.getClass().getMethod(parser.method);
             method.invoke(parser.method, parser.args);
-        }   
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
             throw new RuntimeException(e);
         }
     }
 
     void interested(List<String> args) {
-        
+
     }
 
     void have(List<String> args) {
-        
+
     }
 
     void getpieces(List<String> args) {
@@ -152,18 +186,18 @@ public class Peer {
     }
 
     void data(List<String> args) {
-        
+
     }
 
     void ok(List<String> args) {
-       //do nothing 
+        // do nothing
     }
 
     void peers(List<String> args) {
-        
+
     }
 
     void list(List<String> args) {
-        
+
     }
 }
