7a5ef5a00c46312e8f3bee863be2bd60ed110c38
theogom <theo.gom.2000@gmail.com>
2022-05-20 09:03:18 +0200

    Fix peerServer

--- peer/src/main/java/peer/server/PeerServer.java
+++ peer/src/main/java/peer/server/PeerServer.java
@@ -21,7 +21,7 @@ public class PeerServer extends Thread {
      * @throws IOException If the server socket cannot be created.
      */
     public PeerServer(int port) throws IOException {
-        serverSocket = new ServerSocket(port, 0, InetAddress.getByName("localhost")); // TODO: Use general IP address
+        serverSocket = new ServerSocket(port); // TODO: Use general IP address
     }
 
     /**
