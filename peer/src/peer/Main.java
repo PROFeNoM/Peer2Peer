e74404e456cc17b6b38207022c88403054e06df3
Alexandre Choura <alexandre.choura@bordeaux-inp.fr>
2022-05-20 08:14:52 +0200

    Add ASCII art

--- peer/src/peer/Main.java
+++ peer/src/peer/Main.java
@@ -4,6 +4,15 @@ import peer.util.Configuration;
 
 public class Main {
     public static void main(String[] args) {
+        String asciiArt = " ______ _      _    ___   _____ _                     \n" +
+                          "|  ____(_)    | |  |__ \\ / ____| |                   \n" +
+                          "| |__   _ _ __| |__   ) | (___ | |__   __ _ _ __ ___ \n" +
+                          "|  __| | | '__| '_ \\ / / \\___ \\| '_ \\ / _` | '__/ _ \\\n" +
+                          "| |____| | |  | |_) / /_ ____) | | | | (_| | | |  __/\n" +
+                          "|______|_|_|  |_.__/____|_____/|_| |_|\\__,_|_|  \\___|\n";
+
+        System.out.println(asciiArt);
+
         int peerPort, maxPeers;
         for (String arg : args) {
             System.out.println(arg);
